c9a71d85f5f3df58e54a52851f6f08baa8de6ae7
nexryai <nexryai@sda1.net>
2025-05-07 05:24:47 +0000

    WIP

--- js/src/worker-node.ts
+++ js/src/worker-node.ts
@@ -1,70 +1,70 @@
 import { parentPort } from 'worker_threads';
 import Ikaria from './ikaria-wasm.js';
 
+if (!parentPort) {
+    throw new Error('This module must be run as a worker thread');
+}
 
-parentPort!.on('message', async msg => {
-    const { type, filePath } = msg;
+parentPort.on('message', async (message: any) => {
+    try {
+        const { type, filePath, startSec, endSec, tweakTimestamp } = message;
+        const parentDir = filePath.substring(0, filePath.lastIndexOf('/'));
+        const fileName = filePath.substring(filePath.lastIndexOf('/') + 1);
 
-    const parentDir = filePath.substring(0, filePath.lastIndexOf('/'));
-    const fileName = filePath.substring(filePath.lastIndexOf('/') + 1);
+        const ikaria = await Ikaria();
 
-    let data;
+        if (!ikaria.FS.analyzePath('/work', false).exists) {
+            ikaria.FS.mkdir('/work');
+        }
+        ikaria.FS.mount(ikaria.FS.filesystems.NODEFS, {
+            root: parentDir,
+        }, '/work');
 
-    const ikaria = await Ikaria();
+        switch (type) {
+            case 'get_video_info':
+                // Call the wasm module.
+                const info = ikaria.getVideoInfo('/work/' + fileName);
 
-    switch (type) {
-        case 'get_file_info':
-            // Mount FS for files.
-            if (!ikaria.FS.analyzePath('/work', false).exists) {
-                ikaria.FS.mkdir('/work');
-            }
-            ikaria.FS.mount(ikaria.FS.filesystems.NODEFS, {
-                // mount parent directory of the file path
-                root: parentDir,
-
-            }, '/work');
-
-            ikaria.trimingWebM('/work/' + fileName, '/trimed_' + fileName, "0", "20.000000");
-
-            const trimmedFileBuffer = ikaria.FS.readFile('/trimed_' + fileName);
-
-            // Call the wasm module.
-            const info = ikaria.getVideoInfo('/trimed_' + fileName);
-
-            const keyframes = [];
-            // ref: https://github.com/emscripten-core/emscripten/issues/11070
-            // @ts-ignore
-            for (let i = 0; i < info.keyframes.size(); i++) {
+                const keyframes = [];
+                // ref: https://github.com/emscripten-core/emscripten/issues/11070
                 // @ts-ignore
-                keyframes.push(info.keyframes.get(i).pts_time_string);
-            }
+                for (let i = 0; i < info.keyframes.size(); i++) {
+                    // @ts-ignore
+                    keyframes.push(info.keyframes.get(i).ptsTimeString);
+                }
 
-            const versions = {
-                libavutil:  ikaria.AVUTIL_VERSION(),
-                libavcodec:  ikaria.AVCODEC_VERSION(),
-                libavformat:  ikaria.AVFORMAT_VERSION(),
-            };
+                const versions = {
+                    libavutil: ikaria.AVUTIL_VERSION(),
+                    libavcodec: ikaria.AVCODEC_VERSION(),
+                    libavformat: ikaria.AVFORMAT_VERSION(),
+                };
 
-            // Send back data response.
-            data = {
-                ...info,
-                keyframes: keyframes,
-                versions,
-                trimmedFileBuffer,
-            }
+                // Send back data response.
+                const data = {
+                    ...info,
+                    keyframes,
+                    versions
+                };
 
-            console.log('Worker: File info', data);
-            postMessage(data);
+                console.log('Worker: File info', data);
+                parentPort?.postMessage(data);
+                break;
 
-            // Cleanup mount.
-            ikaria.FS.unmount('/work');
-            break;
-    
-    
-        default:
-            break;
+            case 'trim_video':
+                ikaria.trimingWebM('/work/' + fileName, '/trimed_' + fileName, startSec, endSec, tweakTimestamp);
+                const trimmedFileBuffer = ikaria.FS.readFile('/trimed_' + fileName);
+                parentPort?.postMessage(trimmedFileBuffer);
+                break;
+
+            default:
+                break;
+        }
+
+        // Cleanup mount
+        ikaria.FS.unmount('/work');
+    } catch (error: any) {
+        parentPort?.postMessage({ error: error.message });
     }
-
 });
 
 // self.importScripts('ikaria-wasm.js'); // Load ffprobe into worker context.

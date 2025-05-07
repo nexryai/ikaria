fd8917fd86c62dc2b02be4e8035c44399b70cdbe
nexryai <nexryai@sda1.net>
2025-05-07 05:25:57 +0000

    Worker内で発生した例外をcatchするように

--- js/src/worker-browser.ts
+++ js/src/worker-browser.ts
@@ -3,71 +3,74 @@ import Ikaria from './ikaria-wasm.js';
 console.log('Worker: Starting worker');
 
 self.onmessage = async (e: MessageEvent) => {
-    console.log('Worker: Received message', e.data);
-    const type = e.data[0];
-    const file = e.data[1];
+    try {
+        console.log('Worker: Received message', e.data);
+        const type = e.data[0];
+        const file = e.data[1];
 
-    const ikaria = await Ikaria();
+        const ikaria = await Ikaria();
 
-    switch (type) {
-        case 'get_video_info':
-            // Mount FS for files.
-            if (!ikaria.FS.analyzePath('/work', false).exists) {
-                ikaria.FS.mkdir('/work');
-            }
+        switch (type) {
+            case 'get_video_info':
+                // Mount FS for files.
+                if (!ikaria.FS.analyzePath('/work', false).exists) {
+                    ikaria.FS.mkdir('/work');
+                }
 
-            ikaria.FS.mount(ikaria.FS.filesystems.WORKERFS, { files: [file] }, '/work');
+                ikaria.FS.mount(ikaria.FS.filesystems.WORKERFS, { files: [file] }, '/work');
 
-            // Call the wasm module.
-            const info = ikaria.getVideoInfo('/work/' + file.name);
+                // Call the wasm module.
+                const info = ikaria.getVideoInfo('/work/' + file.name);
 
-            const keyframes = [];
-            // ref: https://github.com/emscripten-core/emscripten/issues/11070
-            // @ts-ignore
-            for (let i = 0; i < info.keyframes.size(); i++) {
+                const keyframes = [];
+                // ref: https://github.com/emscripten-core/emscripten/issues/11070
                 // @ts-ignore
-                keyframes.push(info.keyframes.get(i).ptsTimeString);
-            }
+                for (let i = 0; i < info.keyframes.size(); i++) {
+                    // @ts-ignore
+                    keyframes.push(info.keyframes.get(i).ptsTimeString);
+                }
 
-            // Send back data response.
-            const data = {
-                ...info,
-                keyframes: keyframes,
-            }
+                // Send back data response.
+                const data = {
+                    ...info,
+                    keyframes: keyframes,
+                }
 
-            console.log('Worker: File info', data);
-            postMessage(data);
+                console.log('Worker: File info', data);
+                postMessage(data);
 
-            // Cleanup mount.
-            ikaria.FS.unmount('/work');
-            break;
-    
-        case 'trim_video':
-            const startSec = e.data[2];
-            const endSec = e.data[3];
-            const tweakTimestamp = e.data[4];
-
-            // Mount FS for files.
-            if (!ikaria.FS.analyzePath('/work', false).exists) {
-                ikaria.FS.mkdir('/work');
-            }
-
-            ikaria.FS.mount(ikaria.FS.filesystems.WORKERFS, { files: [file] }, '/work');
-
-            ikaria.trimingWebM('/work/' + file.name, '/trimed_' + file.name, startSec, endSec, tweakTimestamp);
-
-            const trimmedFileBuffer = ikaria.FS.readFile('/trimed_' + file.name);
-            const trimmedFile = new Blob([trimmedFileBuffer], { type: file.type });
-            const blobUrl = URL.createObjectURL(trimmedFile);
-
-            postMessage(blobUrl);
-
-            // Cleanup mount.
-            ikaria.FS.unmount('/work');
-            break;
+                // Cleanup mount.
+                ikaria.FS.unmount('/work');
+                break;
         
-        default:
-            break;
-    }
+            case 'trim_video':
+                const startSec = e.data[2];
+                const endSec = e.data[3];
+                const tweakTimestamp = e.data[4];
 
+                // Mount FS for files.
+                if (!ikaria.FS.analyzePath('/work', false).exists) {
+                    ikaria.FS.mkdir('/work');
+                }
+
+                ikaria.FS.mount(ikaria.FS.filesystems.WORKERFS, { files: [file] }, '/work');
+
+                ikaria.trimingWebM('/work/' + file.name, '/trimed_' + file.name, startSec, endSec, tweakTimestamp);
+
+                const trimmedFileBuffer = ikaria.FS.readFile('/trimed_' + file.name);
+                const trimmedFile = new Blob([trimmedFileBuffer], { type: file.type });
+                const blobUrl = URL.createObjectURL(trimmedFile);
+
+                postMessage(blobUrl);
+
+                // Cleanup mount.
+                ikaria.FS.unmount('/work');
+                break;
+            
+            default:
+                break;
+        }
+    } catch (error: any) {
+        postMessage({ error: error.message });
+    }
 }

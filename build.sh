043e4ac665eb6690a20a28de88f8143a2664cf22
nexryai <nexryai@sda1.net>
2025-05-07 02:44:14 +0000

    Chore: Delete an outdated script

--- build.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p dist
-docker build -t ffprobe-wasm .
-docker create -ti --name ffprobe-wasm-container ffprobe-wasm
-docker cp ffprobe-wasm-container:/build/dist/ www
-docker rm -fv ffprobe-wasm-container

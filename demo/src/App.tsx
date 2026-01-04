8fe5263e8d13e1ee95eb94fafca1c472b6889090
nexryai <61890205+nexryai@users.noreply.github.com>
2026-01-04 12:21:50 +0000

    WIP

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { remuxToDash } from '../../js/dist';
+import { remuxToDash } from '../../js/src';
 import VideoPlayer from './components/VideoPlayer';
 
 const requestPersistentStorage = async (): Promise<boolean> => {
--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -16,6 +16,9 @@ export default defineConfig({
       ]
     }
   },
+  worker: {
+    format: 'es',
+  },
   plugins: [react()],
   preview: {
     headers: {

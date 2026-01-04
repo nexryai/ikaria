3d3248993e3c9cb28a5e855865bd3718cedf79a2
nexryai <61890205+nexryai@users.noreply.github.com>
2026-01-04 09:06:33 +0000

    Switch to Cloudflare Pages

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -4,6 +4,12 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS"
+    },
     fs: {
       allow: [
         "../.."
@@ -11,4 +17,12 @@ export default defineConfig({
     }
   },
   plugins: [react()],
+  preview: {
+    headers: {
+      'Cross-Origin-Opener-Policy': 'same-origin',
+      'Cross-Origin-Embedder-Policy': 'require-corp',
+      "Access-Control-Allow-Origin": "*",
+      "Access-Control-Allow-Methods": "GET, POST, OPTIONS"
+    },
+  },
 })

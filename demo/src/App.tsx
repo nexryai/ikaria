8f483ca030b0d60f7433cec4e27f75bfcdafb437
nexryai <61890205+nexryai@users.noreply.github.com>
2026-01-04 02:45:33 +0000

    Update path

--- demo/src/App.tsx
+++ demo/src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { remuxToDash } from '../../js/src';
+import { remuxToDash } from '../../js/dist';
 import VideoPlayer from './components/VideoPlayer';
 
 /**

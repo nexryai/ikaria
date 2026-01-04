70b697f425904fdeb5b7af16430e4dfbaa044474
nexryai <61890205+nexryai@users.noreply.github.com>
2026-01-04 08:14:45 +0000

    Using coi-serviceworker for demo page

--- demo/vite.config.ts
+++ demo/vite.config.ts
@@ -4,12 +4,6 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   server: {
-    headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin',
-      'Cross-Origin-Embedder-Policy': 'require-corp',
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, OPTIONS"
-    },
     fs: {
       allow: [
         "../.."
@@ -17,12 +11,4 @@ export default defineConfig({
     }
   },
   plugins: [react()],
-  preview: {
-    headers: {
-      'Cross-Origin-Opener-Policy': 'same-origin',
-      'Cross-Origin-Embedder-Policy': 'require-corp',
-      "Access-Control-Allow-Origin": "*",
-      "Access-Control-Allow-Methods": "GET, POST, OPTIONS"
-    },
-  },
 })
